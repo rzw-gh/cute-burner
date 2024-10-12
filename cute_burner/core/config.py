314199efca58b008a48f22efa00d8d937d83fbe4
Reza Gholizadeh <65118978+rzw-gh@users.noreply.github.com>
2024-10-12 23:25:39 +0200

    Update config.py

--- cute_burner/core/config.py
+++ cute_burner/core/config.py
@@ -11,59 +11,11 @@ video_extensions = ['.mp4', '.avi', '.mkv', '.mov', '.flv', '.wmv', '.webm', '.m
 leading_zero = 2  # appends one 0 before video index name e.g: 01
 
 copy_right = [
-    "AniFox.site",  # website
-    r".:: ارائه شده توسط وبسایت انی فاکس ::.\N.:: AniFox.site ::.",  # start of video
-    r"وبسایت انی فاکس با افتخار تقدیم میکند\NAniFox.site",  # middle of video
-    r"انیمه های بیشتر در\Nt.me/AniFoxIR | AniFox.site",  # end of video
+    "mywebsite.com",  # website
+    r"more to watch on mywebsite.com",  # start of video
+    r"more to watch on mywebsite.com",  # middle of video
+    r"more to watch on mywebsite.com",  # end of video
 ]
 
 # include words you want to remove from text of extracted soft subtitle
-words_to_remove_from_subtitle = [
-    "nightmovie", "digimovie", "digimoviez", "avamovie", "aniplus", "animelist", "animlist", "Anime Khor", "AnimWorld", "Forums.AnimWorld.Net",
-    "اوا مووی", "دیجی مووی", "دیجی موویز", "نایت مووی", "انیمه لیست", "انی پلاس", "انیمه ورلد", "انیم ورلد",
-    "شبکه های اجتماعی",
-    "شبکه اجتماعی",
-    "تلگرام", "اینستاگرام", "telegram", "instagram", "پیج", "کانال تلگرام", "تلگرامی", "t.me", "@",
-    "وب سایت",
-    "زیرنویس",
-    ":زیرنویس",
-    "زیرنویس:",
-    "زیر نویس",
-    ":زیر نویس",
-    "زیر نویس:",
-    "با افتخار تقدیم میکند",
-    "ترجمه و تنظیم",
-    ":ترجمه و تنظیم",
-    "ترجمه و تنظیم:",
-    "تنظیم و ترجمه",
-    ":تنظیم و ترجمه",
-    "تنظیم و ترجمه:",
-    "ترجمه و زیرنویس",
-    "ترجمه و زیرنویس:",
-    ":ترجمه و زیرنویس",
-    "ترجمه و زیر نویس",
-    "ترجمه و زیر نویس:",
-    ":ترجمه و زیر نویس",
-    "کاری از تیم",
-    "انکود",
-    "ارائه ای از",
-    "ارائه شده",
-    "ارائه‌شده",
-    "ترجمه‌از",
-    "ترجمه‌و‌تنظیم",
-    "تنظیم‌و‌ترجمه",
-    "کاری‌از",
-    "تیم‌ترجمه",
-    "وب‌سایت",
-    "زیر‌نویس",
-    "تقدیم‌میکند",
-    "انکود‌توسط",
-    "انکود‌اختصاصی",
-    "تیم ترجمه",
-    "ترجمه اختصاصی",
-    "انکودر", "encode", "encoder", "translate", "translator", "translated",
-    ".com", ".ir", ".org", ".shop", ".site", ".fun", ".me", ".top", ".tv",
-    "ترجمه از", "مترجم", "مترجمین",
-    "》", "《", ">", "<", "~", ".::", "::.",
-    "m 151 9 l 155 14 l"
-]
+words_to_remove_from_subtitle = [".com"]
